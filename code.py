8f08b740f77cb93e341fd49380cc98783c4a219e
Poopie <elstuhnofficial@gmail.com>
2021-08-19 09:56:49 +0800

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,9 @@
 from github import Github
-g = Github("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")
+import os
+token = os.getenv('secret')
+g = Github(token)
 
-g = Github(base_url="https://elstuhn/api/v3", login_or_token="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")
+g = Github(base_url="https://elstuhn/api/v3", login_or_token=token)
 
 # Then play with your Github objects:
 for repo in g.get_user().get_repos():
